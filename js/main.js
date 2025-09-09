30cd9fde721594261a7c95d4f97cd990297932c6
HyunRyuuu <ryubifa@gmail.com>
2025-09-09 16:00:01 +0900

    20250909 - btype 슬라이드 영역 추가

--- js/main.js
+++ js/main.js
@@ -12,4 +12,37 @@ $(document).ready(function(){
 	    },
         watchOverflow: true,
     });
+
+    // 요일별 웹툰
+    const weekly_webtoon_lank = new Swiper('.book__slide-wrap.weekly', {
+        slidesPerView: 6,
+		spaceBetween: 6,
+        navigation: {
+         nextEl: ".book__slide-wrap.weekly .book__list-next",
+         prevEl: ".book__slide-wrap.weekly .book__list-prev",
+	    },
+        watchOverflow: true,
+    });
+
+    // 기다리면 무료로 시작해!
+    const wait_webtoon_lank = new Swiper('.book__slide-wrap.wait', {
+        slidesPerView: 6,
+		spaceBetween: 6,
+        navigation: {
+         nextEl: ".book__slide-wrap.wait .book__list-next",
+         prevEl: ".book__slide-wrap.wait .book__list-prev",
+	    },
+        watchOverflow: true,
+    });
+
+    // 오늘, 리디의 발견
+    const today_webtoon_lank = new Swiper('.book__slide-wrap.today', {
+        slidesPerView: 6,
+		spaceBetween: 6,
+        navigation: {
+         nextEl: ".book__slide-wrap.today .book__list-next",
+         prevEl: ".book__slide-wrap.today .book__list-prev",
+	    },
+        watchOverflow: true,
+    });
 });
